c0386d713a980c08a5b85744573671a9110543c7
MrCoolBay <100962538+MrCoolBay@users.noreply.github.com>
2024-03-17 16:48:22 +0100

    Maj BDD

--- models/modelbook.php
+++ models/modelbook.php
@@ -3,7 +3,7 @@
 function DbConnexion()
 {
     try {
-        $db = new PDO('mysql:host=localhost;dbname=mvc;charset=utf8', 'root', '');
+        $db = new PDO('mysql:host=localhost:3306;dbname=fabienlu_books;charset=utf8', 'httpdocs', 'yk9g51BOMG');
         return $db;
     } catch (Exception $e) {
         die('Erreur : ' . $e->getMessage());
